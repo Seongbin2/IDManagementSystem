ca5146eea765ebeae30b198cea1f560d35e4b797
Seongbin <tjdqls0594@naver.com>
2021-03-16 13:17:59 +0900

    new codes

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add User ID");
+		System.out.println("2. Delete User ID");
+		System.out.println("3. Edit User ID");
+		System.out.println("4. View User ID");
 		System.out.println("5. Exit");
 		
 	
