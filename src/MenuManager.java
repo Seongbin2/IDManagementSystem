1e87d949738d3ef01adeca4619848c2abce348dc
Seongbin <tjdqls0594@naver.com>
2021-03-30 18:20:08 +0900

    2021.03.30

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -9,34 +9,65 @@ public class MenuManager {
 		
 		while (num != 6) {
 			
-		System.out.println("Website ID Manage MENU \n********************");
-		System.out.println("1. Add User ID");
-		System.out.println("2. Delete User ID");
-		System.out.println("3. Edit User ID");
-		System.out.println("4. View User ID");
-		System.out.println("5. Show a Menu");
-		System.out.println("6. Exit");
-		System.out.println("Select one number between 1-6");
-		num = input.nextInt();
-		switch (num) {
-		case 1 :
-			System.out.print("Name : ");
-			String User_name = input.next();
-			System.out.print("New ID: ");
-			int User_ID = input.nextInt(); 
-			System.out.print("New Password(only number) : ");
-			int User_password = input.nextInt();
-
-			System.out.print(" : ");
-			break;
-		case 2 :
-			System.out.print ("ID : ");
-			System.out.print ("Password : ");
-			
-			
-		}
+			System.out.println("Website ID Manage MENU \n**********************");
+			System.out.println("1. Add User ID");
+			System.out.println("2. Delete User ID");
+			System.out.println("3. Edit User ID");
+			System.out.println("4. View User ID");
+			System.out.println("5. Show a Menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1-6");
+			num = input.nextInt();
+		
+			if (num == 1) {
+				addID();
+			}
+			else if (num == 2) {
+				deleteID();
+			}
+			else if (num == 3) {
+				editID();
+			}
+			else if (num == 4) {
+				viewID();
+			}
+			else {
+				continue;
+			}
 		}
 	}
+	public static void addID() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Name : ");
+		String User_name = input.next();
+		System.out.print("New ID: ");
+		int User_ID = input.nextInt(); 
+		System.out.print("New Password(only number) : ");
+		int User_password = input.nextInt();
+		System.out.print("E-mail address : ");
+		String User_mail = input.next();
+		System.out.print("Phone Number : ");
+		String user_phonenumber = input.next();
+	}
+
+	public static void deleteID() {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Name : ");
+		String User_name = input.next();
+	}
+	public static void editID () {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Name : ");
+		String User_name = input.next();
+	}
+	
+	public static void viewID () {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Name : ");
+		String User_name = input.next();
+	}
+	
+	
 }
 
 	
