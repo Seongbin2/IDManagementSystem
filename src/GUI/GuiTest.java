1d757aca498e982dbed106417d62ae1591c48732
Seongbin <tjdqls0594@naver.com>
2021-05-25 21:34:43 +0900

    2021.05.25. made GUI

---
+++ src/GUI/GuiTest.java
@@ -0,0 +1,11 @@
+package GUI;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+
+		//serIDAdder useridadder = new UserIDAdder();
+		UserinfoViewer userinfoviewer = new UserinfoViewer();
+	}
+
+}
